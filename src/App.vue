918b4405365ed7baf202209aeb8d56b23979c006
Alessio Forti <info@alessioforti.it>
2022-06-12 19:23:41 +0200

    Isolated submit form logic into component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+
+import AddItemsForm from './components/AddItemsForm.vue'
 import ItemsTemplate from './components/ItemsTemplate.vue'
 
 import { ref } from "vue";
@@ -10,13 +11,12 @@ const items = ref([
   {id: 3, label: 'Sit all day', done: false}
 ])
 
-const newItem = ref('')
 
 const markCompleted = (item) => {
   item.done = !item.done
 }
 
-const addItem = () => {
+const addItem = (newItem) => {
   items.value.push({
     id: items.value.length + 1,
     label: newItem.value,
@@ -30,19 +30,8 @@ const addItem = () => {
 <template>
   <div>
     <h1 class="text-3xl">My Undo List</h1>
-    <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
-    <form @submit.prevent="addItem">
-      <input 
-        v-model="newItem"
-        type="text" 
-        class="border border-gray-300 rounded-md"
-      >
-      <button 
-        class="bg-green-600 ml-4 py-2 px-4 text-bold text-white rounded-md"
-      >
-        +
-      </button>
-    </form>
+
+    <add-items-form @add-item="addItem"></add-items-form>
 
     <items-template :items="items" @done="markCompleted"></items-template>
 
---
+++ src/components/AddItemsForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['addItem'])
+
+const newItem = ref('')
+
+const addItem = () => {
+  emit('addItem', newItem)
+}
+
+</script>
+
+<template>
+  <form @submit.prevent="addItem">
+    <input 
+      v-model="newItem"
+      type="text" 
+      class="border border-gray-300 rounded-md"
+    >
+    <button 
+      class="bg-green-600 ml-4 py-2 px-4 text-bold text-white rounded-md"
+    >
+      +
+    </button>
+  </form>
+</template>
